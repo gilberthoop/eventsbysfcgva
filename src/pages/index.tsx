dc5c09ece74cee8557b3e998b63e75c3b66d1546
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-08-17 16:40:35 -0700

    Refactor Upcoming Events components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,9 @@
-import { useEffect } from "react";
-import { Action } from "redux";
-import { useDispatch, useSelector } from "react-redux";
-import { ThunkDispatch } from "@reduxjs/toolkit";
-import { RootState, fetchEvents } from "@/store";
-import Event from "@/components/Event";
-import { SFCEvent } from "@/types";
+import UpcomingEvents from "@/components/events/UpcomingEvents";
 
 export default function Home() {
-  const { data: events } = useSelector(
-    (state: { events: { data: SFCEvent[] } }) => state.events
-  );
-  const dispatch: ThunkDispatch<RootState, unknown, Action> = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchEvents());
-  }, []);
-
   return (
-    <section className="p-6 sm:px-10 sm:py-10 md:px-16 lg:px-22 lg:py-12">
-      <header className="text-center text-white text-2xl sm:text-4xl my-8">
-        Upcoming Events
-      </header>
-
-      <section className="flex flex-wrap flex-col justify-center sm:flex-row gap-y-2 md:gap-x-8 lg:gap-x-14">
-        {events &&
-          events.map((event, index) => <Event key={index} details={event} />)}
-      </section>
-    </section>
+    <main>
+      <UpcomingEvents />
+    </main>
   );
 }
