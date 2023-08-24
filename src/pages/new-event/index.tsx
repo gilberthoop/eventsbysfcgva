dc110f9d2d56f0c0ab6e0013b0899cbdd937522c
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-08-23 23:02:08 -0700

    Handle creation of a new event

--- src/pages/new-event/index.tsx
+++ src/pages/new-event/index.tsx
@@ -1,18 +1,44 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import useAuth from "@/hooks/use-auth";
-import NewEvent from "@/components/events/NewEvent";
+import Eventform from "@/components/events/EventForm";
+import { SFCEvent } from "@/types";
 
 function NewEventPage() {
   const { jwtToken } = useAuth();
   const router = useRouter();
 
-  useEffect(() => {
-    if (!jwtToken) router.push("/login");
-  }, []);
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  const handleNewEventSubmit = async (eventParams: SFCEvent) => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/events", eventParams);
+      setResponse(response.data?.message);
+      setHasError(false);
+      setLoading(false);
+      router.push("/");
+    } catch (e) {
+      console.error(e);
+      setResponse("Unable to add new event.");
+      setHasError(true);
+    }
+  };
 
   return (
-    <div className="p-6 sm:py-16 sm:px-20">{jwtToken && <NewEvent />}</div>
+    <div className="p-6 sm:py-16 sm:px-20">
+      <Eventform
+        formTitle="Add a new event"
+        action="add"
+        loading={loading}
+        hasError={hasError}
+        response={response}
+        onNewEventSubmit={handleNewEventSubmit}
+      />
+    </div>
   );
 }
 
