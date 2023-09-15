0606e140b91a9d6aae55ec46d868f08ab521a8b5
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-09-14 21:28:17 -0700

    Implement event management admin task

--- src/pages/new-event/index.tsx
+++ src/pages/new-event/index.tsx
@@ -1,10 +1,13 @@
 import useEventRequestHandler from "@/hooks/use-event-request-handler";
+import useAdmin from "@/hooks/use-admin";
 import Eventform from "@/components/events/EventForm";
 import { FormAction } from "@/types";
 
 function NewEventPage() {
   const { handleNewEventSubmit, loading, response, hasError } =
     useEventRequestHandler();
+
+  useAdmin();
 
   return (
     <div className="p-6 sm:py-16 sm:px-20">
