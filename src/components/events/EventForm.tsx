593db1c80e2a3e29a0cbf2cc2d4d714140e661df
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-09-18 14:13:22 -0700

    Refactor typing and UX

--- src/components/events/EventForm.tsx
+++ src/components/events/EventForm.tsx
@@ -6,6 +6,8 @@ import { sanitizeInput } from "@/utils/input-validation";
 import { HOSTS } from "@/utils/global-constants";
 import AppDateTimePicker from "@/components/AppDateTimePicker";
 
+type SubmitNewEvent = (eventParams: SFCEvent) => void;
+type EditEvent = (eventParams: SFCEvent) => void;
 interface EventFormProps {
   formTitle: string;
   formData?: SFCEvent;
@@ -13,8 +15,8 @@ interface EventFormProps {
   loading: boolean;
   hasError: boolean;
   response: string;
-  onNewEventSubmit?: (eventParams: SFCEvent) => void;
-  onEditEventSubmit?: (eventParams: SFCEvent) => void;
+  onNewEventSubmit?: SubmitNewEvent;
+  onEditEventSubmit?: EditEvent;
 }
 
 const EventForm: React.FC<EventFormProps> = ({
@@ -139,107 +141,113 @@ const EventForm: React.FC<EventFormProps> = ({
         <h1 className="text-3xl">{formTitle}</h1>
       </header>
 
-      <form onSubmit={handleSubmit} className="form">
-        {response && (
-          <h2
-            className={`text-center text-lg ${
-              hasError ? "text-red-500" : "text-blue-500"
-            }`}
-          >
-            {response}
-          </h2>
-        )}
-
-        <div className="form__group">
-          <label htmlFor="host">Host:</label>
-          <select
-            name="host"
-            value={formState.host}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="">Select an option</option>
-            {HOSTS.map((host, index) => (
-              <option key={index} value={host}>
-                {host}
-              </option>
-            ))}
-          </select>
+      {loading ? (
+        <div className="flex justify-center mt-20">
+          <CircularProgress size={80} color="info" />
         </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="form">
+          {response && (
+            <h2
+              className={`text-center text-lg ${
+                hasError ? "text-red-500" : "text-blue-500"
+              }`}
+            >
+              {response}
+            </h2>
+          )}
 
-        <div className="form__group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={formState.name}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+          <div className="form__group">
+            <label htmlFor="host">Host:</label>
+            <select
+              name="host"
+              value={formState.host}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Select an option</option>
+              {HOSTS.map((host, index) => (
+                <option key={index} value={host}>
+                  {host}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className="form__group">
-          <label htmlFor="description">Description:</label>
-          <textarea
-            name="description"
-            value={formState.description}
-            onChange={handleInputChange}
-            rows={4}
-            required
-          />
-        </div>
+          <div className="form__group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={formState.name}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="form__group">
-          <label htmlFor="venue">Venue:</label>
-          <input
-            type="text"
-            name="venue"
-            value={formState.venue}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+          <div className="form__group">
+            <label htmlFor="description">Description:</label>
+            <textarea
+              name="description"
+              value={formState.description}
+              onChange={handleInputChange}
+              rows={4}
+              required
+            />
+          </div>
 
-        <div className="form__group">
-          <label htmlFor="address">Address:</label>
-          <input
-            type="text"
-            name="address"
-            value={formState.address}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+          <div className="form__group">
+            <label htmlFor="venue">Venue:</label>
+            <input
+              type="text"
+              name="venue"
+              value={formState.venue}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="form__group mt-3">
-          <AppDateTimePicker
-            label="Select the event start date and time (PST)"
-            scheduleISOformat={formState.isoStartScheduleFormat}
-            onDateChange={onStartDateChange}
-          />
-        </div>
+          <div className="form__group">
+            <label htmlFor="address">Address:</label>
+            <input
+              type="text"
+              name="address"
+              value={formState.address}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="form__group my-3">
-          <AppDateTimePicker
-            label="Select the event end date and time (PST)"
-            scheduleISOformat={formState.isoEndScheduleFormat}
-            onDateChange={onEndDateChange}
-          />
-        </div>
+          <div className="form__group mt-3">
+            <AppDateTimePicker
+              label="Select the event start date and time (PST)"
+              scheduleISOformat={formState.isoStartScheduleFormat}
+              onDateChange={onStartDateChange}
+            />
+          </div>
 
-        <div className="flex justify-between my-4">
-          <button
-            className="form__button form__button--cancel"
-            onClick={cancelEditEvent}
-          >
-            {loading ? <CircularProgress size={24} /> : "Cancel"}
-          </button>
+          <div className="form__group my-3">
+            <AppDateTimePicker
+              label="Select the event end date and time (PST)"
+              scheduleISOformat={formState.isoEndScheduleFormat}
+              onDateChange={onEndDateChange}
+            />
+          </div>
 
-          <button type="submit" className="form__button" disabled={loading}>
-            {loading ? <CircularProgress size={24} /> : "Submit"}
-          </button>
-        </div>
-      </form>
+          <div className="flex justify-between my-4">
+            <button
+              className="form__button form__button--cancel"
+              onClick={cancelEditEvent}
+            >
+              Cancel
+            </button>
+
+            <button type="submit" className="form__button" disabled={loading}>
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
     </main>
   );
 };
