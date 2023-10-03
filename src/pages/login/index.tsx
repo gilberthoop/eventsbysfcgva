a7ca3b527b15113ee1aaecf155f5a58fa6fcb066
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-10-02 19:53:10 -0700

    Add sidebar menu on login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,18 +2,22 @@ import Login from "@/components/admin/Login";
 import useAdmin from "@/hooks/use-admin";
 import useAuthorization from "@/hooks/use-authorization";
 import { LoginParams } from "@/types";
+import SideMenuBar from "@/components/menu/SideMenuBar";
 
 const LoginPage: React.FC = () => {
   const { handleAdminLogin, errors } = useAdmin();
   useAuthorization();
 
   return (
-    <section className="p-6 sm:py-16 sm:px-20">
-      <Login
-        onLogin={(params: LoginParams) => handleAdminLogin(params)}
-        errorMessage={errors}
-      />
-    </section>
+    <div className="block lg:flex">
+      <SideMenuBar />
+      <section className="p-6 sm:py-16 sm:px-20 w-full h-screen">
+        <Login
+          onLogin={(params: LoginParams) => handleAdminLogin(params)}
+          errorMessage={errors}
+        />
+      </section>
+    </div>
   );
 };
 
